6ed29d59d74216d7bc721a9883993139c22a8878
Muhammad Zaka Ullah <97341177+Zaka112@users.noreply.github.com>
2023-05-24 17:05:14 +0200

    unnecessary imports

--- src/components/recipes/RecipeItem.jsx
+++ src/components/recipes/RecipeItem.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import {
   CircularProgress,
   Typography,
